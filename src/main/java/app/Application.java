2ea25bb08bb4ebfed9ca6cbcfeeedfdb2e5a71f9
ygrit0s <ygritos@outlook.com>
2022-12-22 22:51:37 +0900

    Replaced

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,17 @@
+package app;
+
+import app.config.AppConfig;
+import app.model.AnimalsCage;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+		for (int i = 0; i < 5; i++) {
+			var bean = appContext.getBean(AnimalsCage.class);
+			bean.whatAnimalSay();
+		}
+	}
+}
